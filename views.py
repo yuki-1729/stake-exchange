f7ebe2d47ee25ae6f37d89244528d26005e325ba
Yuki <167043764+yuki-1729@users.noreply.github.com>
2024-05-04 21:58:51 +0900

    StakeID設定用のクラスを追加

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ import json
 import asyncio
 import nextcord
 
+# PayPay Login Classes
 class LoginModal(nextcord.ui.Modal):
     def __init__(self, paypay_class):
         super().__init__(
@@ -101,3 +102,48 @@ class LoginOTPModal(nextcord.ui.Modal):
             return
 
         await status_message.edit("ログインしました")
+
+# Set StakeID Class
+class StakeIDModal(nextcord.ui.Modal):
+    def __init__(self, stake_class):
+        super().__init__(
+            title="StakeID 設定",
+            timeout=None
+        )
+
+        self.stake_class = stake_class
+
+        self.stake_id = nextcord.ui.TextInput(
+            label="ID",
+            style=nextcord.TextInputStyle.short,
+            required=True,
+            placeholder="StakeのID(間違えないように注意してください)"
+        )
+        self.add_item(self.stake_id)
+
+    async def callback(
+        self,
+        interaction: nextcord.Interaction
+    ):
+        coro = asyncio.to_thread(self.stake_class.get_user_meta, self.stake_id.value)
+
+        status_message = await interaction.response.send_message("設定中...", ephemeral=True)
+
+        try:
+            result = await coro
+            if result["data"]["user"] == None:
+                await status_message.edit("指定のStakeIDが見つかりませんでした")
+                return
+        except:
+            await status_message.edit("StakeIDの検索に失敗しました")
+            return
+        
+        with open("ids.json", "r", encoding="utf-8", errors="ignore") as file:
+            ids = json.load(file)
+        ids[str(interaction.user.id)] = self.stake_id
+        with open("ids.json", "w", encoding="utf-8", errors="ignore") as file:
+            json.dump(ids, file, indent=4)
+        
+        await status_message.edit("IDを設定しました")
+
+# LTC Sell(販売) Classes
